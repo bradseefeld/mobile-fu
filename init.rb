3fef9c0d2ed30b1c6a1065082c2981546b2be246
Brad Seefeld <bradley.seefeld@gmail.com>
2011-11-23 21:26:38 -0800

    No need to check for action controller and view

--- init.rb
+++ init.rb
@@ -2,7 +2,5 @@ require "mobile_fu"
 require "mobile_fu/rails/controller_methods"
 require "mobile_fu/rails/view_helper"
 
-Rails.logger.info "loading from init"
-
-::ActionController::Base.send(:include, MobileFu::Rails::ControllerMethods)
-::ActionView::Base.send(:include, MobileFu::Rails::ViewHelper)
+ActionController::Base.send(:include, MobileFu::Rails::ControllerMethods)
+ActionView::Base.send(:include, MobileFu::Rails::ViewHelper)
--- lib/mobile_fu/railtie.rb
+++ lib/mobile_fu/railtie.rb
@@ -1,14 +1,11 @@
 module MobileFu
   class Railtie < Rails::Railtie
     config.after_initialize do
-      Rails.logger.info "loading from railtie"
-      if defined? ::ActionController::Base
-        require "mobile_fu/rails/controller_methods"
-        ::ActionController::Base.send(:include, MobileFu::Rails::ControllerMethods)
+      require "mobile_fu/rails/controller_methods"
+      ActionController::Base.send(:include, MobileFu::Rails::ControllerMethods)
         
-        require "mobile_fu/rails/view_helper"
-        ::ActionView::Base.send(:include, MobileFu::Rails::ViewHelper)
-      end
+      require "mobile_fu/rails/view_helper"
+      ActionView::Base.send(:include, MobileFu::Rails::ViewHelper)
     end
   end
 end
