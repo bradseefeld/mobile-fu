522dd327373f722f22b3cf615af9249e2f9afb9f
Brad Seefeld <bradley.seefeld@gmail.com>
2011-11-23 21:08:56 -0800

    Update init.rb to load and include proper files

--- init.rb
+++ init.rb
@@ -2,6 +2,7 @@ require "mobile_fu"
 require "mobile_fu/rails/controller_methods"
 require "mobile_fu/rails/view_helper"
 
-ActionView::Base.send(:include, MobileFu::Rails::ViewHelper)
-ActionView::Base.send(:include, MobilizedStyles)
-ActionView::Base.send(:alias_method_chain, :stylesheet_link_tag, :mobilization)
+Rails.logger.info "loading from init"
+
+::ActionController::Base.send(:include, MobileFu::Rails::ControllerMethods)
+::ActionView::Base.send(:include, MobileFu::Rails::ViewHelper)
--- lib/mobile_fu/railtie.rb
+++ lib/mobile_fu/railtie.rb
@@ -1,13 +1,14 @@
 module MobileFu
   class Railtie < Rails::Railtie
     config.after_initialize do
-      #if defined? ::ActionController::Base
+      Rails.logger.info "loading from railtie"
+      if defined? ::ActionController::Base
         require "mobile_fu/rails/controller_methods"
         ::ActionController::Base.send(:include, MobileFu::Rails::ControllerMethods)
         
         require "mobile_fu/rails/view_helper"
         ::ActionView::Base.send(:include, MobileFu::Rails::ViewHelper)
-      ##end
+      end
     end
   end
 end
